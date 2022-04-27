c56459221ff272e0f910ebed8ab6eefdcba82698
vxvrs <viggooveres@gmail.com>
2022-04-27 17:43:13 +0200

    Split encoder/decoder into separate class functions

--- model2.py
+++ model2.py
@@ -1,9 +1,11 @@
+import time
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as f
 from torchvision import transforms
 
-from data import PolyvoreDataset, Resize, ToTensor
+from data import PolyvoreDataset, Resize, ToTensor, Normalize
 
 
 class AutoEncoder(nn.Module):
@@ -14,32 +16,44 @@ class AutoEncoder(nn.Module):
         self.pool = nn.MaxPool2d(2, 2)
         self.t_conv1 = nn.ConvTranspose2d(4, 16, 2, stride=2)
         self.t_conv2 = nn.ConvTranspose2d(16, 3, 2, stride=2)
-        # Todo: Add dense layers for embedding.
+        # TODO: Add dense layers for embedding.
+        # self.d1 = nn.Linear(4 * 100 * 100, )
 
-    def forward(self, x):
-        x = x.type(torch.FloatTensor)
-
+    def encoder(self, x):
         x = f.relu(self.conv1(x))
         x = self.pool(x)
         x = f.relu(self.conv2(x))
         x = self.pool(x)
+
+        return x
+
+    def decoder(self, x):
         x = f.relu(self.t_conv1(x))
         x = torch.sigmoid(self.t_conv2(x))
 
         return x
 
+    def forward(self, x):
+        x = x.type(torch.FloatTensor)
 
-# TODO: Check loss with normalized dataset.
+        x = self.encoder(x)
+        x = self.decoder(x)
+
+        return x
+
+
 def main():
+    main_start = time.time()
+
     trainset = PolyvoreDataset("polyvore-dataset/train_no_dup.json",
-                               transform=transforms.Compose([Resize(400), ToTensor()]))
+                               transform=transforms.Compose([Resize(400), Normalize(), ToTensor()]))
 
     model = AutoEncoder()
-    criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+    criterion = nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
 
     images = list()
-    for i in range(10):
+    for i in range(len(trainset)):
         image = trainset[i]["image"]
         for img in image:
             images.append(img)
@@ -49,7 +63,8 @@ def main():
     print(data.shape)
 
     n_epochs = 10
-    for epoch in range(1, n_epochs+1):
+    for epoch in range(1, n_epochs + 1):
+        start_time = time.time()
         train_loss = 0.0
         optimizer.zero_grad()
         outputs = model(data)
@@ -58,7 +73,9 @@ def main():
         optimizer.step()
         train_loss += loss.item() * data.size(0)
 
-        print('Epoch: {} \tTraining Loss: {:.6f}'.format(epoch, train_loss))
+        print("Epoch: {} \tTraining Loss: {:.6f} \tTime: {:.2f}".format(epoch, train_loss, time.time() - start_time))
+
+    print("Total time: {:.2f}".format(time.time() - main_start))
 
 
 if __name__ == "__main__":
