9a724b2a70de4e613f86f32b0bfafb74b2f1d380
vxvrs <viggooveres@gmail.com>
2022-05-06 20:27:40 +0200

    Made starter file for autoencoder using clip

---
+++ clip_hybrid.py
@@ -0,0 +1,10 @@
+def main(parse_args):
+    pass
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parse_args = parser.parse_args()
+    main(parse_args)
