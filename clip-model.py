d8ce67bbae5acf11cde23b6b513471d523cca20d
vxvrs <viggooveres@gmail.com>
2022-05-06 14:01:59 +0200

    Started on prepping FITB queries and making train, valid and test sets.

--- clip-model.py
+++ clip-model.py
@@ -1,29 +1,45 @@
 import torch
-from torch.utils.data import DataLoader
+import pandas as pd
 import clip
 from PIL import Image
-from data import PolyvoreDataset, ToTensor
+from random import randint
+from numpy.random import permutation
+from sklearn.model_selection import train_test_split
+
+
+class Config:
+    dataset_path = "./farfetch-dataset"
 
 
 def main():
     device = "cuda" if torch.cuda.is_available() else "cpu"
-    model, preprocess = clip.load("ViT-B/32", device=device, jit=False)
+    # model, preprocess = clip.load("ViT-B/32", device=device, jit=False)
 
-    trainset = PolyvoreDataset("polyvore-dataset/train_no_dup.json")
+    products = pd.read_parquet(f"{Config.dataset_path}/products.parquet", engine="pyarrow")
+    print(products.columns)
 
-    sample = trainset[0]
-    for i, line in enumerate(sample["name"]):
-        img = Image.open(sample["image"][i])
-        img = preprocess(img).unsqueeze(0)
-        token = clip.tokenize(line)
+    outfits = pd.read_parquet(f"{Config.dataset_path}/manual_outfits.parquet", engine="pyarrow")
+    outfits["products_shuffled"] = outfits.apply(lambda row: permutation(row["products"]).tolist(), axis=1)
+    outfits["incomplete_outfit"] = outfits.apply(lambda row: row["products_shuffled"][:-1], axis=1)
+    outfits["missing_product"] = outfits.apply(lambda row: row["products_shuffled"][-1], axis=1)
+    outfits = outfits.drop("products_shuffled", axis=1)
 
-        with torch.no_grad():
-            print(model.encode_image(img).shape, model.encode_text(token).shape, sep='\t\t')
+    train_outfits, test_outfits = train_test_split(outfits, test_size=0.3)
+    train_outfits, valid_outfits = train_test_split(train_outfits, test_size=0.3)
+    print(train_outfits.shape, valid_outfits.shape, test_outfits.shape)
 
-            logits_per_image, logits_per_text = model(img, token)
-            probs = logits_per_image.softmax(dim=-1).cpu().numpy()
-
-            print("Label probs:", probs)
+    # for i, line in enumerate(sample["name"]):
+    #     img = Image.open(sample["image"][i])
+    #     img = preprocess(img).unsqueeze(0)
+    #     token = clip.tokenize(line)
+    #
+    #     with torch.no_grad():
+    #         print(model.encode_image(img).shape, model.encode_text(token).shape, sep='\t\t')
+    #
+    #         logits_per_image, logits_per_text = model(img, token)
+    #         probs = logits_per_image.softmax(dim=-1).cpu().numpy()
+    #
+    #         print("Label probs:", probs)
 
     # with torch.no_grad():
     #     img_f = model.encode_image(image)
