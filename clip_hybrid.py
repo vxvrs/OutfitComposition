ddb148f37018ca3e1ed1fd36c59f7da476368943
vxvrs <viggooveres@gmail.com>
2022-05-06 22:57:04 +0200

    Started with prepping data for clip embedding

--- clip_hybrid.py
+++ clip_hybrid.py
@@ -1,10 +1,25 @@
+import clip
+import pandas as pd
+
+
+def image_text_pair(row):
+    text_columns = ["product_family", "product_category", "product_sub_category", "product_gender", "product_main_colour",
+                    "product_second_color", "product_brand", "product_materials", "product_short_description",
+                    "product_attributes", "product_highlights"]
+    image_column = "product_image_path"
+    return row[image_column]
+
+
 def main(parse_args):
-    pass
+    dataset = pd.read_parquet(f"{parse_args.dataset}/products_train_1651863895.parquet", engine="pyarrow")
+    print(dataset.iloc[3])
+    # print(dataset.apply(lambda row: image_text_pair(row), axis=1).tolist())
 
 
 if __name__ == "__main__":
-    import argparse
+    import argparse, pathlib
 
     parser = argparse.ArgumentParser()
-    parse_args = parser.parse_args()
-    main(parse_args)
+    parser.add_argument("-d", "--dataset", type=pathlib.Path, default="./dataset",
+                        help="Path of directory where products and manual outfits files are stored.")
+    main(parser.parse_args())
