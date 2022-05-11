9ac31463799aeea37c3bd273c5dbbdd5bf5f5f0e
vxvrs <viggooveres@gmail.com>
2022-05-11 18:47:42 +0200

    Changed train, valid, test split to just train, test

--- process_data.py
+++ process_data.py
@@ -28,32 +28,19 @@ def add_fitb_queries(outfits, product_ids):
     return outfits.drop("products_shuffled", axis=1)
 
 
-def split_train_valid_test(data, test_size=0.3, valid_size=0.3):
-    """
-    This function is used to split the data into a training, validation and test set.
-    :param data: Data to be split.
-    :param test_size: Size of the test set relative to all data.
-    :param valid_size: Size of the validation set relative to train set.
-    :return: Returns training, validation and test sets.
-    """
-    train, test = train_test_split(data, test_size=test_size)
-    train, valid = train_test_split(train, test_size=valid_size)
-    return train, valid, test
-
-
 def main(parse_args):
     products = pd.read_parquet(f"{parse_args.dataset}/products.parquet", engine="pyarrow")
-    train_products, valid_products, test_products = split_train_valid_test(products)
+
+    train_products, test_products = train_test_split(products, test_size=0.3)
     if parse_args.debug:
         print(train_products.sample(1).iloc[0])
-        print("Train / valid / test shapes:", train_products.shape, valid_products.shape, test_products.shape)
+        print("Train / test shapes:", train_products.shape, test_products.shape)
 
     unique_name = f"_{int(time.time())}" if parse_args.unique_name else ""
     if parse_args.debug: print(unique_name)
 
     if not parse_args.debug or args.unique_name:
         train_products.to_parquet(f"{parse_args.target}/products_train{unique_name}.parquet")
-        valid_products.to_parquet(f"{parse_args.target}/products_valid{unique_name}.parquet")
         test_products.to_parquet(f"{parse_args.target}/products_test{unique_name}.parquet")
 
     outfits = pd.read_parquet(f"{parse_args.dataset}/manual_outfits.parquet", engine="pyarrow")
@@ -67,10 +54,10 @@ def main(parse_args):
 if __name__ == "__main__":
     import argparse, pathlib
 
-    parser = argparse.ArgumentParser(description="Split the dataset in training, validation and testing sets and "
+    parser = argparse.ArgumentParser(description="Split the dataset in training and testing sets and "
                                                  "generate fill-in-the-blank queries from the manual outfits.",
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("-d", "--dataset", type=pathlib.Path, default="./dataset",
+    parser.add_argument("dataset", type=pathlib.Path,
                         help="Path of directory where products and manual outfits files are stored.")
     parser.add_argument("--unique-name", action="store_true", help="Store the files generated with unique names.")
     parser.add_argument("-t", "--target", type=pathlib.Path, default=".",
