b1bafdc55a427bf0450399aa07bf7492ae43fb6d
vxvrs <viggooveres@gmail.com>
2022-05-18 21:42:30 +0200

    Added shell script for processing data

---
+++ preprocess_products.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+echo "Dataset dir: $1"
+python data_farfetch.py --save "$1/products.parquet"
+python data_farfetch.py --save "$1/products_train.parquet"
+python data_farfetch.py --save "$1/products_test.parquet"
